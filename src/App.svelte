883ec1331b1bb89549e6add640927f2eedaf513b
Jonathan Sarry <jsarry@gmail.com>
2024-03-24 21:35:04 +0200

    Add routing store

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,25 @@
 
   import Layout from "./Layout/Layout.svelte";
   import Main from "./Tabs/Main.svelte";
+  import { tabStore } from "./store";
+
+  let tab: string;
+
+  const unsubscribe = tabStore.subscribe((value) => {
+    tab = value;
+  });
+
+  $: console.log(tab);
 </script>
 
 <Layout>
-  <Main />
+  {#if tab === "home"}
+    <Main />
+  {/if}
+  {#if tab === "about"}
+    <h1>About</h1>
+  {/if}
+  {#if tab === "contact"}
+    <h1>Contact</h1>
+  {/if}
 </Layout>
--- src/Layout/Nav.svelte
+++ src/Layout/Nav.svelte
@@ -1,5 +1,10 @@
 <script lang="ts">
   import Layout from "../Layout/Layout.svelte";
+  import { tabStore } from "../store";
+
+  const changeTab = (tab: string) => {
+    tabStore.set(tab);
+  };
 </script>
 
     <nav >
@@ -7,9 +12,9 @@
             <h1 class="p-4 text-secondary font-bold border-b border-gray-100">Lorem Picsum Recipies</h1>
         </div>
       <ul class="flex justify-between py-4 px-8  bg-slate-50 w-100">
-        <li><button type="button">Home</button></li>
-        <li><button type="button">About</button></li>
-        <li><button type="button">Contact</button></li>
+        <li><button on:click={() => changeTab('home')} type="button">Home</button></li>
+        <li><button on:click={() => changeTab('about')} type="button">About</button></li>
+        <li><button on:click={() => changeTab('contact')} type="button">Contact</button></li>
       </ul>
     </nav>
 
